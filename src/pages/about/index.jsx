7fbdcfe43c9e776f238058c2839604b4bffddb45
unknown <inomjonovsardorbek20011005@gmail.com>
2022-08-16 01:13:39 -0700

    about uploaded

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,19 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../about/Header";
+import Body1 from "../about/Body1";
+import Body2 from "../about/Body2";
+import Body3 from "../about/Body3";
+
+function Index() {
+	return (
+		<Box>
+			<Header />
+			<Body1 />
+			<Body2 />
+			<Body3 />
+		</Box>
+	);
+}
+
+export default Index;
