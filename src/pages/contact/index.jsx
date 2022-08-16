405d9d16231d71c2408dd80791223f588c97bf42
unknown <inomjonovsardorbek20011005@gmail.com>
2022-08-16 01:31:00 -0700

    Contact uploaded

---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,15 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Header from "../contact/Header";
+import Map from "../contact/Map";
+
+function Index() {
+	return (
+		<Box>
+			<Header />
+			<Map />
+		</Box>
+	);
+}
+
+export default Index;
